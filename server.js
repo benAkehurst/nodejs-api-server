6ddeacc9e2f11240361110d838d79a452f913521
benAkehurst <benakehurst@hotmail.com>
2019-02-10 20:55:46 +0000

    Reordered server and added 404 middleware handler

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const Task = require('./api/models/todoListModel');
-const routes = require('./api/routes/todoListRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/YourDB", {
+mongoose.connect("mongodb://localhost:27017/TodoAPI_DB", {
   useNewUrlParser: true
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+const routes = require('./api/routes/todoListRoutes');
 routes(app);
+
+app.use((req, res) => {
+  res.status(404).send({url: req.originalUrl + ' not found'});
+})
 
 
 app.listen(port);
