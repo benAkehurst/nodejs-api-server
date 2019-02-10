0b5186059af0db29d80025e4c5280141f2b3ae32
benAkehurst <benakehurst@hotmail.com>
2019-02-10 20:55:28 +0000

    added api prefix to routes

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -3,11 +3,11 @@ module.exports = function(app) {
   const todoList = require('../controllers/todoListController');
 
   // ToDo Routes
-  app.route('/tasks')
+  app.route('/api/tasks')
   .get(todoList.list_all_tasks)
   .post(todoList.create_a_task);
 
-  app.route('/tasks/:taskId')
+  app.route('/api/tasks/:taskId')
   .get(todoList.read_a_task)
   .put(todoList.update_a_task)
   .delete(todoList.delete_a_task);
