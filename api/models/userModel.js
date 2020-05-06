8ac4e2c46e3e7993fef5c58559bc8a105fb7c69c
benakehurst <benakehurst@hotmail.com>
2020-05-06 10:30:16 +0100

    feat(usermodel): update the user model
    took out user status and changed to have a boolean value for active
    added timestamps on model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,31 +2,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: false
-  },
-  email: {
-    type: String,
-    unique: true
-  },
-  password: {
-    type: String,
-    unique: true
-  },
-  created_date: {
-    type: Date,
-    default: Date.now
-  },
-  status: {
-    type: [{
+const UserSchema = new Schema(
+  {
+    name: {
       type: String,
-      enum: ['pending', 'aproved', 'deleted']
-    }],
-    default: ['aproved']
-  }
-});
-
+      required: false,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+    },
+    userActive: {
+      type: Boolean,
+      default: [true],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', UserSchema);
