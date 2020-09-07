50083c579cd79c4b7a1f11b53d971ade6c83208e
Ben <benakehurst@hotmail.com>
2020-09-07 10:43:59 +0100

    conforms todolist routes

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -2,16 +2,13 @@
 module.exports = (app) => {
   const todoList = require('../controllers/todoListController');
 
-  // ToDo Routes
   app.route('/api/tasks/create-new-task/:token').post(todoList.create_new_task);
-
   app
-    .route('/api/tasks/list-all-tasks/:token/:userId')
-    .get(todoList.list_all_tasks);
-
+    .route('/api/tasks/read-all-user-tasks/:userId/:token')
+    .get(todoList.read_all_user_tasks);
   app
-    .route('/api/tasks/:token/:taskId:/:userId')
-    .get(todoList.read_a_task)
-    .put(todoList.update_a_task)
-    .delete(todoList.delete_a_task);
+    .route('/api/tasks/:userId/:token/:taskId')
+    .get(todoList.read_single_task)
+    .put(todoList.update_single_task)
+    .delete(todoList.delete_single_task);
 };
