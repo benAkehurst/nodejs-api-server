37103438b1d9260a06f06453cd3cff2c92080a51
Ben <benakehurst@hotmail.com>
2020-09-07 10:40:02 +0100

    update middlewares

---
+++ middlewares/validators.js
@@ -0,0 +1,53 @@
+/**
+ * isValidEmail helper method
+ * @param {string} email
+ * @returns {Boolean} True or False
+ */
+const validateEmail = (email) => {
+  const regEx = /\S+@\S+\.\S+/;
+  return regEx.test(email);
+};
+
+/**
+ * validatePassword helper method
+ * @param {string} password
+ * @returns {Boolean} True or False
+ */
+const validatePassword = (password) => {
+  if (password.length <= 6 || password === '') {
+    return false;
+  }
+  return true;
+};
+/**
+ * isEmpty helper method
+ * @param {string, integer} input
+ * @returns {Boolean} True or False
+ */
+const isEmpty = (input) => {
+  if (input === undefined || input === '') {
+    return true;
+  }
+  if (input.replace(/\s/g, '').length) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * empty helper method
+ * @param {string, integer} input
+ * @returns {Boolean} True or False
+ */
+const empty = (input) => {
+  if (input === undefined || input === '') {
+    return true;
+  }
+};
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+  isEmpty,
+  empty,
+};
