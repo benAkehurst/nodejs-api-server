12d390f41f70d27d4bfe4d62ebf9fac67d143166
Ben <benakehurst@hotmail.com>
2020-09-07 10:43:50 +0100

    add token check to auth routes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -3,4 +3,7 @@ module.exports = (app) => {
   const authController = require('../controllers/authController');
   app.route('/api/auth/create-new-user').post(authController.create_new_user);
   app.route('/api/auth/login-user').post(authController.login_user);
+  app
+    .route('/api/auth/check-token-valid/:token')
+    .get(authController.check_token_valid);
 };
