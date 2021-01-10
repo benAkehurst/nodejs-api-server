3bc66c019eeb6a1667843f4f6d22b5b28d418341
Ben <benakehurst@hotmail.com>
2021-01-10 18:39:48 +0000

    Adds dev flag for db connection

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 // Server Dependencies
-const express = require("express");
-const http = require("http");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Models Imports
-const User = require("./api/models/userModel");
-const Task = require("./api/models/taskModel");
+const User = require('./api/models/userModel');
+const Task = require('./api/models/taskModel');
+const Code = require('./api/models/codeModel');
 
 // Init Express
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 
 // DB Connection
 mongoose.Promise = global.Promise;
+let dev = process.env.DEV;
 mongoose.connect(
-  // `mongodb://${process.env.DB_CONNECT}`,
-  `mongodb://localhost:27017/default_db`,
+  dev
+    ? `mongodb://${process.env.LOCAL_DB}`
+    : `mongodb://${process.env.PROD_DB}`,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -28,11 +31,11 @@ mongoose.connect(
     if (e) {
       const dbError = {
         error: e,
-        msg: "Error Connecting to Database. Please check MongoDB is running",
+        msg: 'Error Connecting to Database. Please check MongoDB is running',
       };
       console.log(dbError);
     } else {
-      console.log("Connected to Database");
+      console.log(`Connected to ${dev ? 'Development' : 'Prod'} Database`);
     }
   }
 );
@@ -43,34 +46,34 @@ app.use(bodyParser.json());
 
 // Cors Controls
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PATCH, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'POST, GET, PATCH, DELETE, OPTIONS'
   );
   next();
 });
 app.use(cors());
 
 // Routes Definitions
-const adminRoutes = require("./api/routes/adminRoutes");
-const authRoutes = require("./api/routes/authRoutes");
-const taskRoutes = require("./api/routes/todoListRoutes");
+const adminRoutes = require('./api/routes/adminRoutes');
+const authRoutes = require('./api/routes/authRoutes');
+const taskRoutes = require('./api/routes/todoListRoutes');
 adminRoutes(app);
 authRoutes(app);
 taskRoutes(app);
 
 // 404 Handling
 app.use((req, res) => {
-  res.status(404).send({ url: req.originalUrl + " not found" });
+  res.status(404).send({ url: req.originalUrl + ' not found' });
 });
 
 // Server Port Controls
-const port = process.env.PORT || "3000";
-app.set("port", port);
+const port = process.env.PORT || '3000';
+app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
