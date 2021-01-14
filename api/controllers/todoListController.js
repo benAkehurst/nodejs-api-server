e6cfcb83f1f453521c0887e4a421d50bc87a3714
Ben <benakehurst@hotmail.com>
2021-01-14 16:18:19 +0000

    update all methods

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -9,56 +9,57 @@ const Task = require('../models/taskModel');
  * POST
  * {
  *  "task": "",
- *  "uuid": ""
  * }
  */
 exports.create_new_task = async (req, res) => {
-  const { uuid, token, task } = req.body;
-  if (!uuid || !token || !task) {
+  const { uniqueId, token } = req.params;
+  const { task } = req.body;
+  if (!uniqueId || !token || !task) {
     res.status(400).json({
       success: false,
       message: 'Missing request data',
       data: null,
     });
-  }
-  try {
-    let tokenValid = await tokenMiddleware.checkToken(token);
-    let userExists = await checkUserExists(uuid);
-    if (tokenValid.success && userExists) {
-      let newTask = new Task({
-        task: task,
-        user: uuid,
-        createdOnDate: format(new Date(), 'dd/MM/yyyy'),
-        createdOnTime: format(new Date(), 'HH:mm'),
-      });
-      await newTask.save((err, task) => {
-        if (err) {
-          res.status(400).json({
-            success: false,
-            message: `Couldn't save new task.`,
-            data: err,
-          });
-        } else {
-          res.status(200).json({
-            success: false,
-            message: `Task saved successfully.`,
-            data: task,
-          });
-        }
-      });
-    } else {
+  } else {
+    try {
+      let tokenValid = await tokenMiddleware.checkToken(token);
+      let userExists = await checkUserExists(uniqueId);
+      if (tokenValid.success && userExists) {
+        let newTask = new Task({
+          task: task,
+          user: uniqueId,
+          createdOnDate: format(new Date(), 'dd/MM/yyyy'),
+          createdOnTime: format(new Date(), 'HH:mm'),
+        });
+        await newTask.save((err, task) => {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              message: `Couldn't save new task.`,
+              data: err,
+            });
+          } else {
+            res.status(200).json({
+              success: false,
+              message: `Task saved successfully.`,
+              data: task,
+            });
+          }
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          message: `Couldn't find user or token invalid.`,
+          data: null,
+        });
+      }
+    } catch {
       res.status(400).json({
         success: false,
-        message: `Couldn't find user or token invalid.`,
+        message: 'Something went wrong creating a task - General Error',
         data: null,
       });
     }
-  } catch {
-    res.status(400).json({
-      success: false,
-      message: 'Something went wrong creating a task - General Error',
-      data: null,
-    });
   }
 };
 
@@ -68,45 +69,46 @@ exports.create_new_task = async (req, res) => {
  * PARAMS: /:uniqueId /:token
  */
 exports.read_all_user_tasks = async (req, res) => {
-  const { uuid, token } = req.params;
-  if (!uuid || !token) {
+  const { uniqueId, token } = req.params;
+  if (!uniqueId || !token) {
     res.status(400).json({
       success: false,
       message: 'Missing request data',
       data: null,
     });
-  }
-  try {
-    let tokenValid = await tokenMiddleware.checkToken(token);
-    let userExists = await checkUserExists(uuid);
-    if (tokenValid.success && userExists) {
-      Task.find({ user: { $all: uuid } }, (err, tasks) => {
-        if (err) {
-          return res.status(400).json({
-            success: false,
-            message: 'No tasks fround.',
-            data: err,
+  } else {
+    try {
+      let tokenValid = await tokenMiddleware.checkToken(token);
+      let userExists = await checkUserExists(uniqueId);
+      if (tokenValid.success && userExists) {
+        Task.find({ user: { $all: uniqueId } }, (err, tasks) => {
+          if (err) {
+            return res.status(400).json({
+              success: false,
+              message: 'No tasks fround.',
+              data: err,
+            });
+          }
+          return res.status(200).json({
+            success: true,
+            message: 'Tasks fround.',
+            data: tasks,
           });
-        }
-        return res.status(200).json({
-          success: true,
-          message: 'Tasks fround.',
-          data: tasks,
         });
-      });
-    } else {
+      } else {
+        res.status(400).json({
+          success: false,
+          message: `Couldn't find user or token invalid.`,
+          data: null,
+        });
+      }
+    } catch {
       res.status(400).json({
         success: false,
-        message: `Couldn't find user or token invalid.`,
+        message: 'Something went wrong fetching all tasks - General Error',
         data: null,
       });
     }
-  } catch {
-    res.status(400).json({
-      success: false,
-      message: 'Something went wrong fetching all tasks - General Error',
-      data: null,
-    });
   }
 };
 
@@ -116,45 +118,46 @@ exports.read_all_user_tasks = async (req, res) => {
  * PARAMS: /:uniqueId /:token /:taskId
  */
 exports.read_single_task = async (req, res) => {
-  const { uuid, token, taskId } = req.params;
-  if (!uuid || !token || !taskId) {
+  const { uniqueId, token, taskId } = req.params;
+  if (!uniqueId || !token || !taskId) {
     res.status(400).json({
       success: false,
       message: 'Missing request data',
       data: null,
     });
-  }
-  try {
-    let tokenValid = await tokenMiddleware.checkToken(token);
-    let userExists = await checkUserExists(uuid);
-    if (tokenValid.success && userExists) {
-      Task.findById(taskId, (err, task) => {
-        if (err) {
-          res.status(400).json({
-            success: false,
-            message: "Couldn't find task",
-            data: err,
+  } else {
+    try {
+      let tokenValid = await tokenMiddleware.checkToken(token);
+      let userExists = await checkUserExists(uniqueId);
+      if (tokenValid.success && userExists) {
+        Task.findById(taskId, (err, task) => {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              message: "Couldn't find task",
+              data: err,
+            });
+          }
+          res.status(200).json({
+            success: true,
+            message: 'Single Task found',
+            data: task,
           });
-        }
-        res.status(200).json({
-          success: true,
-          message: 'Single Task found',
-          data: task,
         });
-      });
-    } else {
+      } else {
+        res.status(400).json({
+          success: false,
+          message: `Couldn't find user or token invalid.`,
+          data: null,
+        });
+      }
+    } catch {
       res.status(400).json({
         success: false,
-        message: `Couldn't find user or token invalid.`,
+        message: 'Something went wrong reading single task - General Error',
         data: null,
       });
     }
-  } catch {
-    res.status(400).json({
-      success: false,
-      message: 'Something went wrong reading single task - General Error',
-      data: null,
-    });
   }
 };
 
@@ -167,51 +170,52 @@ exports.read_single_task = async (req, res) => {
  * }
  */
 exports.update_single_task = async (req, res) => {
-  const { uuid, token, taskId } = req.params;
+  const { uniqueId, token, taskId } = req.params;
   const { task } = req.body;
-  if (!uuid || !token || !taskId || !task) {
+  if (!uniqueId || !token || !taskId || !task) {
     res.status(400).json({
       success: false,
       message: 'Missing request data',
       data: null,
     });
-  }
-  try {
-    let tokenValid = await tokenMiddleware.checkToken(token);
-    let userExists = await checkUserExists(uuid);
-    if (tokenValid.success && userExists) {
-      Task.updateOne(
-        { _id: taskId },
-        { $set: { task: task } },
-        { new: true },
-        (err, task) => {
-          if (err) {
-            res.status(400).json({
-              success: false,
-              message: "Couldn't update single task.",
-              data: err,
+  } else {
+    try {
+      let tokenValid = await tokenMiddleware.checkToken(token);
+      let userExists = await checkUserExists(uniqueId);
+      if (tokenValid.success && userExists) {
+        Task.updateOne(
+          { _id: taskId },
+          { $set: { task: task } },
+          { new: true },
+          (err, task) => {
+            if (err) {
+              res.status(400).json({
+                success: false,
+                message: "Couldn't update single task.",
+                data: err,
+              });
+            }
+            res.status(200).json({
+              success: true,
+              message: 'Single task updated successfully.',
+              data: task,
             });
           }
-          res.status(200).json({
-            success: true,
-            message: 'Single task updated successfully.',
-            data: task,
-          });
-        }
-      );
-    } else {
+        );
+      } else {
+        res.status(400).json({
+          success: false,
+          message: `Couldn't find user or token invalid.`,
+          data: null,
+        });
+      }
+    } catch {
       res.status(400).json({
         success: false,
-        message: `Couldn't find user or token invalid.`,
+        message: 'Something went wrong updating single task - General Error',
         data: null,
       });
     }
-  } catch {
-    res.status(400).json({
-      success: false,
-      message: 'Something went wrong updating single task - General Error',
-      data: null,
-    });
   }
 };
 
@@ -221,44 +225,45 @@ exports.update_single_task = async (req, res) => {
  * PARAMS: /:uniqueId /:token /:taskId
  */
 exports.delete_single_task = async (req, res) => {
-  const { uuid, token, taskId } = req.params;
-  if (!uuid || !token || !taskId) {
+  const { uniqueId, token, taskId } = req.params;
+  if (!uniqueId || !token || !taskId) {
     res.status(400).json({
       success: false,
       message: 'Missing request data',
       data: null,
     });
-  }
-  try {
-    let tokenValid = await tokenMiddleware.checkToken(token);
-    let userExists = await checkUserExists(uuid);
-    if (tokenValid.success && userExists) {
-      Task.remove({ _id: taskId }, { new: true }, (err, task) => {
-        if (err) {
-          res.status(400).json({
-            success: false,
-            message: "Couldn't delete task",
-            data: err,
+  } else {
+    try {
+      let tokenValid = await tokenMiddleware.checkToken(token);
+      let userExists = await checkUserExists(uniqueId);
+      if (tokenValid.success && userExists) {
+        Task.remove({ _id: taskId }, { new: true }, (err, task) => {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              message: "Couldn't delete task",
+              data: err,
+            });
+          }
+          res.status(200).json({
+            success: true,
+            message: 'Task deleted successfully',
+            data: task,
           });
-        }
-        res.status(200).json({
-          success: true,
-          message: 'Task deleted successfully',
-          data: task,
         });
-      });
-    } else {
+      } else {
+        res.status(400).json({
+          success: false,
+          message: `Couldn't find user or token invalid.`,
+          data: null,
+        });
+      }
+    } catch {
       res.status(400).json({
         success: false,
-        message: `Couldn't find user or token invalid.`,
+        message: 'Something went wrong reading single task - General Error',
         data: null,
       });
     }
-  } catch {
-    res.status(400).json({
-      success: false,
-      message: 'Something went wrong reading single task - General Error',
-      data: null,
-    });
   }
 };
